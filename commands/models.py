70b3ea99a98a35df4e8ef36ce738935df83d09d8
bigbyto <bigbyto@gmail.com>
2024-06-04 02:14:35 +0800

    chore: model fast switch

--- commands/models.py
+++ commands/models.py
@@ -3,15 +3,38 @@ from telebot.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from utils.md2tgmd import escape
 from context import profiles, session, config
 
+SHORT_NAME = {
+    "gpt4": "gpt-4",
+    "gpt4_32k": "gpt-4-32k",
+    "gpt4_16k": "gpt-4-16k",
+    "0314": "gpt-4-0314",
+    "0613": "gpt-4-0613",
+    "1106": "gpt-4-1106-preview",
+    "0125": "gpt-4-0125-preview",
+    "0409": "gpt-4-turbo-2024-04-09",
+    "gpt4o": "gpt-4o",
+}
+
 
 async def handle_models(message: Message, bot: AsyncTeleBot):
     uid = str(message.from_user.id)
     profile = profiles.load(uid)
+    endpoint = config.get_endpoint(profile.get("endpoint", "None"))
+    text = message.text.replace("/models", "").strip()
+    models = endpoint.get("models", [])
+
+    # fast switch
+    if len(text) > 0:
+        if text in models:
+            await do_model_change(bot, text, message.message_id, message.chat.id, uid, message)
+            return
+        elif SHORT_NAME[text] in models:
+            await do_model_change(bot, SHORT_NAME[text], message.message_id, message.chat.id, uid, message)
+            return
+
     context = f'{message.message_id}:{message.chat.id}:{uid}'
     keyboard = []
     items = []
-
-    endpoint = config.get_endpoint(profile.get("endpoint", "None"))
 
     for model in endpoint.get("models", []):
         callback_data = f'{action["name"]}:{model}:{context}'
@@ -47,7 +70,7 @@ async def do_model_change(bot: AsyncTeleBot, operation: str, msg_id: int, chat_i
         )
         return
 
-    await bot.delete_message(chat_id, msg_id)
+    # await bot.delete_message(chat_id, msg_id)
 
     await bot.send_message(
         chat_id=chat_id,
@@ -63,7 +86,7 @@ def register(bot: AsyncTeleBot, decorator) -> None:
 
 action = {
     "name": 'models',
-    "description": 'list all models',
+    "description": 'list models: /models [model_name]',
     "handler": do_model_change,
     "delete_after_invoke": True
 }
