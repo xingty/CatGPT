d908cad5f5c56f02dbece0fd7559e532832d6cf3
bigbyto <bigbyto@gmail.com>
2024-06-14 15:53:59 +0800

    chore: optimize code structure for models

--- commands/models.py
+++ commands/models.py
@@ -2,7 +2,9 @@ from telebot.async_telebot import AsyncTeleBot
 from telebot.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 from utils.md2tgmd import escape
-from context import profiles, config, get_bot_name
+from context import profiles, config, get_bot_name, Endpoint
+from storage import types
+
 
 SHORT_NAME = {
     "gpt4": "gpt-4",
@@ -14,6 +16,7 @@ SHORT_NAME = {
     "0125": "gpt-4-0125-preview",
     "0409": "gpt-4-turbo-2024-04-09",
     "gpt4o": "gpt-4o",
+    "4o": "gpt-4o",
 }
 
 
@@ -28,13 +31,45 @@ async def handle_models(message: Message, bot: AsyncTeleBot):
     # fast switch
     if len(text) > 0:
         if text in models:
-            await do_model_change(bot, text, message.message_id, message.chat.id, uid, message)
+            await _do_model_change(
+                bot=bot,
+                profile=profile,
+                model=text,
+                msg_ids=[message.message_id],
+                chat_id=message.chat.id,
+                uid=uid
+            )
             return
         elif SHORT_NAME.get(text, None) in models:
-            await do_model_change(bot, SHORT_NAME[text], message.message_id, message.chat.id, uid, message)
+            await _do_model_change(
+                bot=bot,
+                profile=profile,
+                model=SHORT_NAME[text],
+                msg_ids=[message.message_id],
+                chat_id=message.chat.id,
+                uid=uid
+            )
             return
 
-    context = f'{message.message_id}:{message.chat.id}:{uid}'
+    await display_models(
+        bot=bot,
+        profile=profile,
+        endpoint=endpoint,
+        message_id=message.message_id,
+        chat_id=message.chat.id,
+        uid=uid
+    )
+
+
+async def display_models(
+        bot: AsyncTeleBot,
+        profile: types.Profile,
+        endpoint: Endpoint,
+        message_id: int,
+        chat_id: int,
+        uid: int
+):
+    context = f'{message_id}:{chat_id}:{uid}'
     keyboard = []
     items = []
 
@@ -51,7 +86,7 @@ async def handle_models(message: Message, bot: AsyncTeleBot):
 
     msg_text = f'current endpoint: `{profile.endpoint or "None"}`\ncurrent model: `{profile.model or "None"}`\n'
     await bot.send_message(
-        chat_id=message.chat.id,
+        chat_id=chat_id,
         text=escape(msg_text),
         parse_mode="MarkdownV2",
         reply_markup=InlineKeyboardMarkup(keyboard)
@@ -64,35 +99,54 @@ async def do_model_change(bot: AsyncTeleBot, operation: str, msg_ids: list[int],
         return
 
     profile = await profiles.load(uid)
-    message_id = msg_ids[0]
-    endpoint = config.get_endpoint(profile.endpoint or "None")
-    if endpoint is None:
-        await bot.send_message(
-            chat_id=chat_id,
-            reply_to_message_id=message_id,
-            parse_mode="MarkdownV2",
-            text=escape(f'endpoint not found')
-        )
-        return
+    await _do_model_change(
+        bot=bot,
+        profile=profile,
+        model=operation,
+        msg_ids=msg_ids + [message.message_id],
+        chat_id=chat_id,
+        uid=uid
+    )
 
-    if operation not in (endpoint.models or []):
-        await bot.send_message(
-            chat_id=chat_id,
-            reply_to_message_id=message_id,
-            parse_mode="MarkdownV2",
-            text=escape(f'current endpoint does not support the model `{operation}`')
-        )
-        return
 
-    profile.model = operation
-    await profiles.update_model(uid, profile.model)
+async def _do_model_change(
+        bot: AsyncTeleBot,
+        profile: types.Profile,
+        model: str,
+        msg_ids: list[int],
+        chat_id: int,
+        uid: int
+):
+    if profile.model != model:
+        message_id = msg_ids[0]
+        endpoint = config.get_endpoint(profile.endpoint or "None")
+        if endpoint is None:
+            await bot.send_message(
+                chat_id=chat_id,
+                reply_to_message_id=message_id,
+                parse_mode="MarkdownV2",
+                text=escape(f'endpoint not found')
+            )
+            return
+
+        if model not in (endpoint.models or []):
+            await bot.send_message(
+                chat_id=chat_id,
+                reply_to_message_id=message_id,
+                parse_mode="MarkdownV2",
+                text=escape(f'current endpoint does not support the model `{model}`')
+            )
+            return
+
+        profile.model = model
+        await profiles.update_model(uid, profile.model)
 
     await bot.send_message(
         chat_id=chat_id,
         parse_mode="MarkdownV2",
-        text=escape(f'current model: `{operation}`')
+        text=escape(f'current model: `{model}`')
     )
-    await bot.delete_messages(chat_id, [message_id, message.message_id])
+    await bot.delete_messages(chat_id, msg_ids)
 
 
 def register(bot: AsyncTeleBot, decorator, action_provider):
