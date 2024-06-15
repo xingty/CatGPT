b3ee30d1d71731139b25590bc914dda16cb43acf
bigbyto <bigbyto@gmail.com>
2024-06-15 19:41:00 +0800

    update

--- assets/session_schema.sql
+++ assets/session_schema.sql
@@ -27,15 +27,21 @@ create index idx_msg_topic_id on message (topic_id);
 
 create table profile
 (
-    uid  INTEGER,
-    model TEXT,
-    endpoint TEXT,
-    prompt TEXT,
-    private INTEGER,
-    channel INTEGER,
-    groups INTEGER,
-    blocked INTEGER,
+    uid         INTEGER,
+    model       TEXT,
+    endpoint    TEXT,
+    prompt      TEXT,
+    private     INTEGER,
+    channel     INTEGER,
+    groups      INTEGER,
+    blocked     INTEGER,
     unique (uid)
 );
 
 create index idx_profile_uid on profile (uid);
+
+create table version
+(
+    version_name    TEXT
+    version_code    INTEGER
+)
